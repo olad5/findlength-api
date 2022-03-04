ae64822c600316e38f1b613ce203c1b34a6db0f1
Francis Shonubi <shonubioladipo@gmail.com>
2022-03-04 22:39:56 +0100

    feat: program returns speed for single playlist
    - refactored timeComputation functions to better represent what they
      are doing
    - added duration package
    - program now outputs the formatted speeds for both videos and playlists

--- src/controllers/appcontroller.ts
+++ src/controllers/appcontroller.ts
@@ -1,28 +1,36 @@
 import Ytdl from '../services/ytdlOps'
 import express from 'express';
 import debug from 'debug';
-import {getVideoSpeeds} from '../utils/timeComputation';
+import {getResourceSpeed} from '../utils/timeComputation';
 
 const log: debug.IDebugger = debug('app:users-controller');
 
 class AppController {
     /**
-     * Gets the speeds of a single video at 1.25x, 1.5x, 1.75x, 2x speeds
+     * Gets the speed of a single video at 1.25x, 1.5x, 1.75x, 2x speeds
      */
     async getVideoInfo(req: express.Request, res: express.Response) {
-        let videoInfo = await Ytdl.getVideoInfo(req.body.url)// this works for single videos
-        let lengthSeconds: number = Number(videoInfo.videoDetails.lengthSeconds);
-
-        const data: string[] = getVideoSpeeds(lengthSeconds)
-
+        const videoInfo = await Ytdl.getVideoInfo(req.body.url)// this works for single videos
+        const lengthSeconds: number = Number(videoInfo.videoDetails.lengthSeconds);
+        const data: string[] = getResourceSpeed(lengthSeconds)
         return res.status(200).json({status: true, message: "Video speeds computed", videoSpeeds: data});
-
     }
 
+    /**
+     * Gets the speed of a playlist at 1.25x, 1.5x, 1.75x, 2x speeds
+     */
     async getPlaylistInfo(req: express.Request, res: express.Response) {
-        let playlistInfo = await Ytdl.getPlaylistInfo(req.body.url)
-        return res.status(200).json({status: true, message: "Playlist Info", playlistInfo: playlistInfo});
-
+        const playlistInfo = await Ytdl.getPlaylistInfo(req.body.url)
+        let totalSeconds: number = 0
+        // gets the total seconds for the playlist at normal speed
+        for (let i: number = 0; i < playlistInfo.estimatedItemCount; i++) {
+            let durationInSeconds: number | null = playlistInfo.items[i].durationSec
+            if (durationInSeconds) {
+                totalSeconds += durationInSeconds
+            }
+        }
+        const data: string[] = getResourceSpeed(totalSeconds)
+        return res.status(200).json({status: true, message: "Playlist Info", playlistInfo: data});
     }
 }
 
--- src/utils/timeComputation.ts
+++ src/utils/timeComputation.ts
@@ -1,26 +1,59 @@
+const Duration = require("duration");
+
 /**
- * Computes the speeds of a single video at 1.25x, 1.5x, 1.75x, 2x speeds
- */
-function computeVideoSpeed(lengthInSeconds: number, speed: number) {
-    const secondsAtspeed: number = Math.floor(lengthInSeconds / speed) // 6 41
-    let time = new Date(0, 0, 0, 0, 0, secondsAtspeed)
-    let hours: number = time.getHours(); //
-    let minutes: number = time.getMinutes(); // 
-    let seconds: number = time.getSeconds(); //
-    let duration: string = hours <= 0 ? `${minutes}:${seconds}` : `${hours}:${minutes}:${seconds}`; //
-    return duration;
+ * Computes the speeds of a single video/playlist at 1.25x, 1.5x, 1.75x, 2x speeds
+**/
+function computeResourceSpeed(lengthInSeconds: number, speed: number) {
+    // gets the seconds at a particular speed in integers
+    const secondsAtspeed: number = Math.floor(lengthInSeconds / speed)
+
+    let originalDuration = new Date(0, 0, 0, 0, 0, secondsAtspeed)
+
+    // the total duration using the duration module
+    let totalDuration = new Duration(new Date(0, 0, 0, 0, 0, 0), originalDuration);
+
+    // each of the durations for different time points
+    const durationYear: [number, string] = [totalDuration.year, 'years'];
+    const durationMonth: [number, string] = [totalDuration.month, 'months'];
+    const durationDay: [number, string] = [totalDuration.day, 'days'];
+    const durationHour: [number, string] = [totalDuration.hour, 'hours'];
+    const durationMinute: [number, string] = [totalDuration.minute, 'minutes'];
+    const durationSecond: [number, string] = [totalDuration.second, 'seconds'];
+    const durationMillisecond: [number, string] = [totalDuration.millisecond, 'milliseconds'];
+
+
+    let outputString: string = ''
+
+    const timeDurations: [number, string][] = [
+        durationYear,
+        durationMonth,
+        durationDay,
+        durationHour,
+        durationMinute,
+        durationSecond,
+        durationMillisecond
+    ]
+
+    // checks if a duration point is zero, so it doesn't add that to the computed string
+    for (let i = 0; i < timeDurations.length; i++) {
+        let currentDuration: number = timeDurations[i][0];
+        if (currentDuration > 0) {
+            outputString += `${currentDuration}${timeDurations[i][1]}, `
+        }
+    }
+    outputString = outputString.slice(0, outputString.length - 2) //removes the comma and space
+    return outputString;
 }
 
 /**
- * Returns the computed speeds of a single video as an array to the controller
- */
-export function getVideoSpeeds(seconds: number): string[] {
+  Returns the computed speeds of a single video/playlist as an array to the controller
+**/
+export function getResourceSpeed(seconds: number): string[] {
     let defaultSpeeds: number[] = [1.25, 1.5, 1.75, 2]
     let computedSpeeds: string[] = []
 
     defaultSpeeds.forEach(speed => {
-        computedSpeeds.push(computeVideoSpeed(seconds, speed))
+        computedSpeeds.push(computeResourceSpeed(seconds, speed))
     })
-
     return computedSpeeds
 }
