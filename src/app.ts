decc34d5fbe4b10c2b1d20c738215c8286430bac
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-22 10:40:12 +0100

    chore: add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import "express-async-errors"; // package to catch async errors
 import bodyParser from "body-parser";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimiter from "express-rate-limit";
+import compression from "compression";
+
 import process from "process";
 import * as expressWinston from "express-winston";
 import errorHandlerMiddleware from "./middlewares/error-handler";
@@ -9,19 +13,27 @@ import loggerOptions from "./utils/logger";
 import { AppRoutes } from "./routes/approutes";
 
 const app: express.Application = express();
-// const debugLog: debug.IDebugger = debug("app");
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
 
 app.use(bodyParser.json({ limit: "31mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors()); // Implement CORS
+app.use(helmet());
+app.use(cors());
 
 if (!process.env.DEBUG) {
   loggerOptions.meta = true; // when not debugging, make true
 }
 
+app.use(compression());
+
 app.use(expressWinston.logger(loggerOptions));
 
-// const routes: AppRoutes[] = [new AppRoutes(app)]; // add the routes
 new AppRoutes(app); // add the routes
 
 app.use(errorHandlerMiddleware);
